a1869ea69da74d40787cf17bcb1fc5c1a6fef76a
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-09-23 15:14:39 -0500

    need to make a model feature branch

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'Please enter email'],
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectsId,
+                ref: 'Friend'
+            }
+        ]
+    }
+);
