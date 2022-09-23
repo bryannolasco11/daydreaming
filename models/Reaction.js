7f324f3489751f01f67c86ffd79d1dab446c2690
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-09-23 16:08:31 -0500

    reaction model done.  Bugs?

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,2 +1,27 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, Types } = require('mongoose');
 
+const reactionSchema = new Schema(
+    {
+        //custom id to avoid confusion with comment id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+    }
+);
+
+module.exports = reactionSchema;
